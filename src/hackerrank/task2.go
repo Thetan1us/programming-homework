c93c0f992fe136c824c2df584cc6afbaf513af6d
Mykyta Horbenko <geeksa40@gmail.com>
2026-05-11 22:26:17 +0300

    complete task 2

---
+++ src/hackerrank/task2.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func staircase(n int32) {
+	for i := int32(1); i <= n; i++ {
+
+		for j := int32(0); j < n-i; j++ {
+			fmt.Print(" ")
+		}
+
+		for k := int32(0); k < i; k++ {
+			fmt.Print("#")
+		}
+
+		fmt.Println()
+	}
+}
+
+func main() {
+	var n int32 = 6
+	staircase(n)
+	// Юніт тести не додавав, оскільки функція не повертає значень
+}
